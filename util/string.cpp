5d633f0ae06db97dbb012e4040ddcbc36257fc05
gyoon <inticoy0406@gmail.com>
2024-01-20 16:17:10 +0900

    feat: stoi in util

---
+++ util/string.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   String.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gyoon <gyoon@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/20 15:47:09 by gyoon             #+#    #+#             */
+/*   Updated: 2024/01/20 16:05:45 by gyoon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "util/string.hpp"
+
+using namespace Hafserv;
+
+std::pair<bool, int> util::string::stoi(const std::string &str)
+{
+	std::pair<bool, int> ret = std::make_pair(false, 0);
+	std::stringstream ss(str);
+	ss >> ret.second;
+	ret.first = !ss.fail() && !ss.bad() && ss.eof();
+	return ret;
+}
+
+bool hasSpace(const std::string &str)
+{
+	for (int i = 0; i < str.length(); i++)
+		if (std::isspace(str[i]))
+			return true;
+	return false;
+};
+
+std::string toLower(const std::string &str)
+{
+	std::string ret = str;
+	for (int i = 0; i < str.length(); i++)
+		ret[i] = std::tolower(str[i]);
+	return ret;
+}
